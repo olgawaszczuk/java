dd1fab8efb91622a9dc718819651fd124b04ed72
olgawaszczuk <waszczuko@op.pl>
2018-06-06 08:31:32 +0200

    Update CsvFileParser.java

--- CsvFileParser.java
+++ CsvFileParser.java
@@ -13,14 +13,15 @@ public class CsvFileParser implements Serializable {
         obiektOut = null;
     }
 
-   public String readString() throws IOException {
-        BufferedReader in =
+    public String readString() throws IOException {
+        try{
+            BufferedReader in =
                 new BufferedReader(
                         new FileReader(obiektIn));
-        try{
+
             char c;
             StringBuffer strng = new StringBuffer();
-            while ((c = (char) in.read()) != (char) -1)//'\n')
+            while ((c = (char) in.read()) != (char) -1)
                 strng.append(c);
             strng.append('\n');
             in.close();
@@ -33,48 +34,33 @@ public class CsvFileParser implements Serializable {
 
         }
 
+
         return stringMatrix;
 
     }
 
     public Matrix convertToMatrix() {
         ArrayList<ArrayList<Double>> resultData = new ArrayList<ArrayList<Double>>();
-        String[] lines = stringMatrix.split("\n");
-        int i = 0;
-        for (String line : lines) {
-            String[] fields = line.split(";");
-            resultData.add(new ArrayList<Double>());
-            int j = 0;
-            for (String field : fields) {
-                Double v = Double.parseDouble(field);
-                resultData.get(i).add(v);
-                j++;
+        try {
+            String[] lines = stringMatrix.split("\n");
+            int i = 0;
+            for (String line : lines) {
+                String[] fields = line.split(";");
+                resultData.add(new ArrayList<Double>());
+                int j = 0;
+                for (String field : fields) {
+                    Double v = Double.parseDouble(field);
+                    resultData.get(i).add(v);
+                    j++;
+                }
+                i++;
             }
-            i++;
+        }
+        catch (NullPointerException e){
+            return new Matrix(null);
         }
 
         return new Matrix(resultData);
     }
-
-    /*
-
-    public void FileOpenOut() throws IOException {
-        obiektOut = new ObjectOutputStream(
-                new FileOutputStream("abc.txt"));
-    }
-
-    public String getObiektIn() {
-        return obiektIn;
-    }
-
-
-    public void FileCloseOut() throws IOException {
-        obiektOut.close();
-    }
-
-    public ObjectOutputStream getObiektOut() {
-        return obiektOut;
-    }
-
-*/
+    
 }
